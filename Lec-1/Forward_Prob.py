705806b58a884a3ec90e7704f127c3b5da8a131c
EpicFailure-afk <as3704921@gmail.com>
2025-02-25 23:34:51 +0200

    code updated

--- Lec-1/Forward_Prob.py
+++ Lec-1/Forward_Prob.py
@@ -1,51 +1,53 @@
-def tanh(x):
-    e_pos = (2.71828 ** x)
-    e_neg = (2.71828 ** -x)
-    return (e_pos - e_neg) / (e_pos + e_neg)
+def custom_random(low, high, seed):
+    seed = (seed * 9301 + 49297) % 233280
+    return low + (seed / 233280.0) * (high - low)
 
-def initialize_weights(rows, cols):
-    weights = []
-    for _ in range(rows):
-        neuron_weights = []
-        for _ in range(cols):
-            # Generate random value between -0.5 and 0.5 manually
-            rand_val = ((123456789 * 987654321) % 1000000) / 1000000 - 0.5
-            neuron_weights.append(rand_val)
-        weights.append(neuron_weights)
-    return weights
+def custom_exponential(x, terms):
+    res = 1
+    fact = 1
+    pow_x = 1
+    count = 1
+    while count < terms:
+        fact *= count
+        pow_x *= x
+        res += pow_x / fact
+        count += 1
+    return res
 
-def forward_propagation(X, W1, B1, W2, B2, W3, B3):
-    # First hidden layer
-    Z1 = []
-    for neuron, b in zip(W1, B1):
-        sum_val = sum(x * w for x, w in zip(X, neuron)) + b
-        Z1.append(sum_val)
-    A1 = [tanh(z) for z in Z1]
-    
-    # Second hidden layer
-    Z2 = []
-    for neuron, b in zip(W2, B2):
-        sum_val = sum(a * w for a, w in zip(A1, neuron)) + b
-        Z2.append(sum_val)
-    A2 = [tanh(z) for z in Z2]
-    
-    # Output layer
-    Z3 = []
-    for neuron, b in zip(W3, B3):
-        sum_val = sum(a * w for a, w in zip(A2, neuron)) + b
-        Z3.append(sum_val)
-    A3 = [tanh(z) for z in Z3]  
-    
-    return A3
+def custom_tanh(x):
+    exp_x = custom_exponential(x, 10)
+    exp_neg_x = custom_exponential(-x, 10)
+    return (exp_x - exp_neg_x) / (exp_x + exp_neg_x)
 
-X = [0.5, 0.8]  
+seed = 42
+values = []
+inc = 0
+while inc < 6:
+    values.append(custom_random(-0.5, 0.5, seed + inc))
+    inc += 1
 
-W1 = initialize_weights(2, 2)
-B1 = [0.5, 0.5]  
-W2 = initialize_weights(2, 2)
-B2 = [0.7, 0.7]  
-W3 = initialize_weights(1, 2)
-B3 = [((123456789 * 987654321) % 1000000) / 1000000 - 0.5]  
+w1, w2, w3, w4, w5, w6 = values
+b1, b2 = 0.5, 0.7
+i1, i2 = 0.05, 0.1
+target_o1, target_o2 = 0.1, 0.99
 
-output = forward_propagation(X, W1, B1, W2, B2, W3, B3)
-print("Output:", output)
+h1_input = w1 * i1 + w2 * i2 + b1
+h2_input = w3 * i1 + w4 * i2 + b1
+h1_output = custom_tanh(h1_input)
+h2_output = custom_tanh(h2_input)
+
+o1_input = w5 * h1_output + w6 * h2_output + b2
+o2_input = w5 * h1_output + w6 * h2_output + b2
+o1_output = custom_tanh(o1_input)
+o2_output = custom_tanh(o2_input)
+
+E_o1 = 0.5 * (target_o1 - o1_output) * (target_o1 - o1_output)
+E_o2 = 0.5 * (target_o2 - o2_output) * (target_o2 - o2_output)
+total_loss = E_o1 + E_o2
+
+print("Hidden Outputs: h1 =", h1_output, ", h2 =", h2_output)
+print("Output Values: o1 =", o1_output, ", o2 =", o2_output)
+print("Losses: E_o1 =", E_o1, ", E_o2 =", E_o2)
+print("Total Loss:", total_loss)
+
+
